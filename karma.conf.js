423ef090a754586cea60ee8566cb63264112ffb2
Dmitry Sorin <info@staypositive.ru>
2014-09-24 23:54:57 +0400

    Run Chrome in Travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,5 @@
 module.exports = function (config) {
-    config.set({
+    var configuration = {
         frameworks: ['jasmine'],
 
         files: [
@@ -17,29 +17,23 @@ module.exports = function (config) {
             'karma-jasmine',
             'karma-chrome-launcher',
             'karma-firefox-launcher'
-        ]
+        ],
+
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        }
 
         // browsers: ['Chrome', 'ChromeCanary', 'Firefox']
-    });
+    };
+
+    // run chrome in travis
+    // @link https://github.com/karma-runner/karma/issues/1144
+    if (process.env.TRAVIS) {
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
 };
-
-// // Karma configuration
-// // Generated on Tue Sep 16 2014 01:17:35 GMT+0400 (MSK)
-
-// module.exports = function(config) {
-//   config.set({
-
-//     // base path that will be used to resolve all patterns (eg. files, exclude)
-//     basePath: '',
-
-
-//     // frameworks to use
-//     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-//     frameworks: ['jasmine', 'requirejs'],
-
-
-//     // Continuous Integration mode
-//     // if true, Karma captures browsers, runs the tests and exits
-//     singleRun: false
-//   });
-// };
