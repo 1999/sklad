2baca8b38448a34c089a2e78ac86ce3f1e3ce556
Paul Kilmurray <paul@kilbot.com.au>
2016-02-21 16:16:30 +0800

    add modern browser matrix

--- tests/browsers.js
+++ tests/browsers.js
@@ -1,25 +1,113 @@
 module.exports = {
-    sl_chrome: {
+    // Chrome
+    sl_chrome_47_osx_10_11: {
         base: 'SauceLabs',
         browserName: 'chrome',
-        platform: 'Windows 7',
-        version: '35'
+        platform: 'OS X 10.11',
+        version: '47'
     },
-    sl_firefox: {
+    sl_chrome_dev_osx_10_11: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'OS X 10.11',
+        version: 'dev'
+    },
+    sl_chrome_47_windows_10: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Windows 10',
+        version: '47'
+    },
+    sl_chrome_dev_windows_10: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Windows 10',
+        version: 'dev'
+    },
+    sl_chrome_47_linux: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Linux',
+        version: '47'
+    },
+    sl_chrome_dev_linux: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Linux',
+        version: 'dev'
+    },
+
+    // Firefox
+    sl_firefox_43_osx_10_11: {
         base: 'SauceLabs',
         browserName: 'firefox',
-        version: '30'
+        platform: 'OS X 10.11',
+        version: '43.0'
     },
-    sl_ios_safari: {
+    sl_firefox_dev_osx_10_11: {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'OS X 10.11',
+        version: 'dev'
+    },
+    sl_firefox_43_windows_10: {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'Windows 10',
+        version: '43.0'
+    },
+    sl_firefox_dev_windows_10: {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'Windows 10',
+        version: 'dev'
+    },
+    sl_firefox_43_linux: {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'Linux',
+        version: '43.0'
+    },
+    sl_firefox_dev_linux: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Linux',
+        version: 'dev'
+    },
+
+    // Safari
+    sl_safari_9_0_osx_10_11: {
+        base: 'SauceLabs',
+        browserName: 'safari',
+        platform: 'OS X 10.11',
+        version: '9.0'
+    },
+    sl_safari_9_2_ios_10_10: {
         base: 'SauceLabs',
         browserName: 'iphone',
-        platform: 'OS X 10.9',
-        version: '7.1'
+        platform: 'OS X 10.10',
+        version: '9.2'
     },
-    sl_ie_11: {
+
+    // Edge
+    sl_edge_20_10240_windows_10: {
         base: 'SauceLabs',
-        browserName: 'internet explorer',
-        platform: 'Windows 8.1',
-        version: '11'
+        browserName: 'MicrosoftEdge',
+        platform: 'Windows 10',
+        version: '20.10240'
+    },
+
+    // Android Browser
+    sl_android_4_4: {
+        base: 'SauceLabs',
+        browserName: 'android',
+        platform: 'Linux',
+        version: '5.1'
+    },
+    sl_android_4_4: {
+        base: 'SauceLabs',
+        browserName: 'android',
+        platform: 'Linux',
+        version: '4.4'
     }
 }
