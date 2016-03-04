b48640632fecc2a9601fb44a67d0c5e74d04e6db
Dmitry Sorin <dsorin@yandex-team.ru>
2016-03-04 12:20:48 +0300

    Fix tests everywhere except Safari

--- tests/browsers.js
+++ tests/browsers.js
@@ -1,43 +1,43 @@
 module.exports = {
-    // // Chrome
-    // chrome_stable_osx: {
-    //     base: 'SauceLabs',
-    //     browserName: 'chrome',
-    //     platform: 'OS X 10.11',
-    //     version: '48'
-    // },
-    // chrome_stable_win: {
-    //     base: 'SauceLabs',
-    //     browserName: 'chrome',
-    //     platform: 'Windows 10',
-    //     version: '48'
-    // },
-    // chrome_stable_linux: {
-    //     base: 'SauceLabs',
-    //     browserName: 'chrome',
-    //     platform: 'Linux',
-    //     version: '48'
-    // },
+    // Chrome
+    chrome_stable_osx: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'OS X 10.11',
+        version: '48'
+    },
+    chrome_stable_win: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Windows 10',
+        version: '48'
+    },
+    chrome_stable_linux: {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        platform: 'Linux',
+        version: '48'
+    },
 
-    // // Firefox
-    // firefox_stable_osx: {
-    //     base: 'SauceLabs',
-    //     browserName: 'firefox',
-    //     platform: 'OS X 10.11',
-    //     version: '44'
-    // },
-    // firefox_stable_win: {
-    //     base: 'SauceLabs',
-    //     browserName: 'firefox',
-    //     platform: 'Windows 10',
-    //     version: '44'
-    // },
-    // firefox_stable_linux: {
-    //     base: 'SauceLabs',
-    //     browserName: 'firefox',
-    //     platform: 'Linux',
-    //     version: '44'
-    // },
+    // Firefox
+    firefox_stable_osx: {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'OS X 10.11',
+        version: '44'
+    },
+    firefox_stable_win: {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'Windows 10',
+        version: '44'
+    },
+    firefox_stable_linux: {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        platform: 'Linux',
+        version: '44'
+    },
 
     // Safari
     // safari_9_osx_10_11: {
@@ -72,9 +72,9 @@ module.exports = {
     },
 
     // Android Browser
-    // android_5_1: {
-    //     base: 'SauceLabs',
-    //     browserName: 'android',
-    //     version: '5.1'
-    // }
+    android_5_1: {
+        base: 'SauceLabs',
+        browserName: 'android',
+        version: '5.1'
+    }
 }
