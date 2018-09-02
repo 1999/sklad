39016a3e9991ea38ff8d989d5a66e02df62cd3a5
Dmitrii Sorin <dsorin@atlassian.com>
2018-09-02 21:33:59 +1000

    Bump all dependencies including new babel Export sklad as a named variable Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,11 @@
 'use strict';
 
-const webpack = require('webpack');
-const IS_PRODUCTION_BUILD = (process.env.NODE_ENV === 'production');
-
-module.exports = {
+module.exports = (env, { mode }) => ({
     context: __dirname,
-    entry: ['babel-polyfill', './lib/sklad.js'],
+    entry: './lib/sklad.js',
     output: {
         path: `${__dirname}/dist`,
-        filename: IS_PRODUCTION_BUILD ? 'sklad.min.js' : 'sklad.uncompressed.js',
+        filename: (mode === 'production') ? 'sklad.min.js' : 'sklad.uncompressed.js',
         library: 'sklad',
         libraryTarget: 'umd',
     },
@@ -23,20 +20,18 @@ module.exports = {
                     options: {
                         presets: [
                             [
-                                'env',
+                                '@babel/preset-env',
                                 {
                                     targets: {
                                         browsers: ['last 2 versions', 'ie >= 11', 'safari >= 9']
                                     },
-                                    // for uglifyjs...
-                                    forceAllTransforms: IS_PRODUCTION_BUILD,
                                 }
                             ],
                         ],
-                        plugins: ['add-module-exports', 'transform-es2015-typeof-symbol']
+                        plugins: ['add-module-exports']
                     }
                 }
             }
         ]
     },
-};
+});
